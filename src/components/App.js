af9ee564b2aa937de6e36eeebe69748fead3c6e6
NihitaLele <nihitalele@gmail.com>
2024-10-23 19:53:17 +0530

    assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './../styles/App.css';
+import WeatherDisplay from "./WeatherDisplay";
+
+const def = {temperature : 0, conditions : "cool"}
 
 const App = () => {
+
+  const [data, setData] = useState(def);
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setData({ temperature: 15, conditions: "Sunny" })
+    }, 1000)
+  })
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <WeatherDisplay data = {data} />
     </div>
   )
 }
---
+++ src/components/WeatherDisplay.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const WeatherDisplay = ({data}) => {
+
+   // const {data} = props;
+
+   const tempStyle = {color : data.temperature > 20 ? "red" : "blue"}
+
+  return (
+    <div>
+        <p>Temperature : <span style ={tempStyle}> {data.temperature}</span></p>
+        <p>Conditions : {data.conditions}</p>
+    </div>
+  )
+}
+
+export default WeatherDisplay
